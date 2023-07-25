40993c233af625f7111d5da389fb96e05fbe200e
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2023-07-25 17:24:00 +0300

    bug-fixed

--- constansts/ExpensesOutput/ExpensesOutput.js
+++ constansts/ExpensesOutput/ExpensesOutput.js
@@ -6,31 +6,31 @@ const DUMMY_EXPENSES = [
     {
         id: 'e1',
         description: 'T-shirt',
-        price: 49.99,
+        amount: 49.99,
         data: new Date('2022-12-01')
     },
     {
         id: 'e2',
         description: 'T-shirt',
-        price: 49.99,
+        amount: 49.99,
         data: new Date('2022-12-01')
     },
     {
         id: 'e3',
         description: 'Apples',
-        price: 9.99,
+        amount: 9.99,
         data: new Date('2022-12-22')
     },
     {
         id: 'e4',
         description: 'A book',
-        price: 10,
+        amount: 10,
         data: new Date('2022-02-12')
     },
     {
         id: 'e5',
         description: 'A book',
-        price: 10,
+        amount: 10,
         data: new Date('2022-04-12')
     },
 ]
